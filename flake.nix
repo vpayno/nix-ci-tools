8a90da5b572788ac34b50e3f1d76994ab7d97d83
Victor Payno <vpayno@users.noreply.github.com>
2025-05-21 19:05:47 -0700

    nix: add .#usage app

--- flake.nix
+++ flake.nix
@@ -20,11 +20,68 @@
 
   outputs =
     {
+      self,
+      nixpkgs,
       flake-utils,
       treefmt-conf,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      formatter = treefmt-conf.formatter.${system};
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pname = "nix-ci-tools";
+        version = "20250521.0.0";
+        name = "${pname}-${version}";
+
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        usageMessage = ''
+          Available ${name} flake commands:
+
+            nix run .#usage
+        '';
+
+        metadata = {
+          homepage = "https://github.com/vpayno/nix-ci-tools";
+          description = "My CI tools wrapped in a Nix Flake";
+          license = with pkgs.lib.licenses; [ mit ];
+          # maintainers = with pkgs.lib.maintainers; [vpayno];
+          maintainers = [
+            {
+              email = "vpayno@users.noreply.github.com";
+              github = "vpayno";
+              githubId = 3181575;
+              name = "Victor Payno";
+            }
+          ];
+          mainProgram = "showUsage";
+        };
+      in
+      {
+        formatter = treefmt-conf.formatter.${system};
+
+        packages = rec {
+          default = showUsage;
+
+          # very odd, this doesn't work with pkgs.writeShellApplication
+          # odd quoting error when the string usagemessage as new lines
+          showUsage = pkgs.writeShellScriptBin "showUsage" ''
+            printf "%s" "${usageMessage}"
+          '';
+        };
+
+        apps = rec {
+          default = usage;
+
+          usage = {
+            type = "app";
+            pname = "usage";
+            inherit version;
+            name = "${pname}-${version}";
+            program = "${pkgs.lib.getExe self.packages.${system}.showUsage}";
+            meta = metadata;
+          };
+        };
+      }
+    );
 }
